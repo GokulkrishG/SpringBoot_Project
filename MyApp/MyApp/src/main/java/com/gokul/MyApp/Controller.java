e25f261e9db6408acfbd973180ad1f30c50d9110
Gokula Krishnan S <101918408+GokulkrishG@users.noreply.github.com>
2025-03-04 23:49:04 +0530

    Day 4 of Spring Boot
    GET PUt POST DELETE Methods

--- MyApp/MyApp/src/main/java/com/gokul/MyApp/Controller.java
+++ MyApp/MyApp/src/main/java/com/gokul/MyApp/Controller.java
@@ -1,8 +1,7 @@
 package com.gokul.MyApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,10 +9,30 @@ import java.util.List;
 public class Controller {
 
     @Autowired
-    Service service;
+    private Service service;
 
     @RequestMapping("/products")
     public List<Product> product(){
        return service.getProducts();
     }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        service.addProducrt(prod);
+    }
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod) {
+        service.updateProducrt(prod);
+    }
+    @DeleteMapping("/products/{prodId}")
+    public Product deleteProductById(@PathVariable int prodId){
+         return service.deleteProductById(prodId);
+    }
 }
+
+
--- MyApp/MyApp/src/main/java/com/gokul/MyApp/Service.java
+++ MyApp/MyApp/src/main/java/com/gokul/MyApp/Service.java
@@ -1,19 +1,52 @@
 package com.gokul.MyApp;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 @org.springframework.stereotype.Service
 public class Service {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(1, "Pen", 30),
             new Product(2, "Pencil", 5),
-            new Product(3, "Notebbok", 50));
+            new Product(3, "Notebbok", 50)));
 
     public List<Product> getProducts() {
         return products;
     }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getId() == prodId)
+                .findFirst().orElse(new Product(10, "Book", 25));
+    }
+
+
+    public void addProducrt(Product prod) {
+        products.add(prod);
+    }
+
+    public void updateProducrt(Product prod) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == prod.getId())
+                index = i;
+        }
+        products.set(index, prod);
+    }
+
+    public Product deleteProductById(int prodId) {
+
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getId() == prodId)
+                index = i;
+        }
+        return products.remove(index);
+    }
 }
+
