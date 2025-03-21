e86eae367195e3d53b360b469edaca2c9ae841bc
Gokula Krishnan S <101918408+GokulkrishG@users.noreply.github.com>
2025-03-21 21:52:32 +0530

    Bcrypt Password Encoder

--- security/src/main/java/com/gokul/security/config/SecurityConfig.java
+++ security/src/main/java/com/gokul/security/config/SecurityConfig.java
@@ -10,8 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -37,8 +41,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         provider.setUserDetailsService(userDetailsService );
         return provider;
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("admin")
+                .password("{noop}password") // NoOpPasswordEncoder allows plain text passwords
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
--- security/src/main/java/com/gokul/security/service/UserService.java
+++ security/src/main/java/com/gokul/security/service/UserService.java
@@ -3,14 +3,20 @@ package com.gokul.security.service;
 import com.gokul.security.model.Users;
 import com.gokul.security.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepo repo;
 
+    public BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+
     public Users register(Users users){
+        users.setPassword(encoder.encode(users.getPassword()));
         return repo.save(users);
     }
 }
